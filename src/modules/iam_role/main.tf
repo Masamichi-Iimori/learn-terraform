58f302dd152f3854c27010dfbb48b41011876904
飯盛正慶 <iimorimasamichi@masamichi.local>
2021-03-31 15:24:06 +0900

    add ecs, alb, acm, cloudwatch

--- src/modules/iam_role/main.tf
+++ src/modules/iam_role/main.tf
@@ -1,38 +1,25 @@
-variable "name" {}
-variable "policy" {}
-variable "identifier" {}
-
 resource "aws_iam_role" "default" {
-  name = var.name
-  assume_role_policy = data.aws_iam_role_policy_document.assume_role.json
-  
+  name               = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-data "aws_iam_policy_document" "assume_role"{
+data "aws_iam_policy_document" "assume_role" {
   statement {
-    actions = ["sts::AssumeRole"]
+    actions = ["sts:AssumeRole"]
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = [var.identifier]
     }
   }
 }
 
 resource "aws_iam_policy" "default" {
-  name = var.name
-  policy  = var.policy
+  name   = var.name
+  policy = var.policy
 }
 
 resource "aws_iam_role_policy_attachment" "default" {
-  role = aws_iam_role.default.name
+  role       = aws_iam_role.default.name
   policy_arn = aws_iam_policy.default.arn
-}
-
-output "iam_role_arn" {
-  value = aws_iam_role.default.arn
-}
-
-output "iam_role_name" {
-  value = aws_iam_role.default.name
 }
